b8bc6a924ba617ab4b88b71ae9f8296a5ed35ef0
raisumit <Sumit.Rai@teknopoint.local>
2022-12-12 22:11:09 +0530

    background color change for easy and hard handled through useref in this commit as this does not require any data to update using ref insted of state

--- src/app.js
+++ src/app.js
@@ -4,13 +4,16 @@ import HeaderCompo from "./project/guess-who/header";
 import NavCompo from "./project/guess-who/nav";
 import HintOptionCompo from "./project/guess-who/hint-option";
 
+const levelContext = React.createContext();
 const App = () => {
     return (
-     <>
-     <HeaderCompo heading="Guess who ?"/>
-     <NavCompo />
-     <HintOptionCompo />
-     </>
+        <>
+            <HeaderCompo heading="Guess who ?" />
+            <levelContext.Provider value={options}>
+                <NavCompo />
+                <HintOptionCompo />
+            </levelContext.Provider>
+        </>
     );
 }
 
--- src/project/guess-who/nav/nav.jsx
+++ src/project/guess-who/nav/nav.jsx
@@ -1,24 +1,34 @@
-import React from "react";
-function setLevel(e){
+import React, { useRef, useState, useEffect } from "react";
 
-}
 const Nav = () => {
-
+    const EasyLevel = useRef(null);
+    const hardLevel = useRef(null);
+   
     return (
         <nav>
-        <ol className="nav">
-            <li className="nav-link nav-hover">Reset</li>
-            <li onClick={setLevel} className="nav-link nav-hover level"> Easy </li>
-            <li onClick={setLevel} className="nav-link nav-hover"> Hard </li>
+            <ol className="nav">
+                <li  className="nav-link nav-hover">Reset</li>
+                <li ref={EasyLevel} onClick={setLevel} className="nav-link nav-hover level"> Easy </li>
+                <li ref={hardLevel} onClick={setLevel} className="nav-link nav-hover"> Hard </li>
 
-            <li className="nav-link nave-three nav-hover">
-                <select id="select">
-                    <option value="w-img">Word To Image</option>
-                    <option value="img-w">Image To Word</option>
-                </select>
-            </li>
-        </ol>
-    </nav>
-    )
+                <li className="nav-link nave-three nav-hover">
+                    <select id="select">
+                        <option value="w-img">Word To Image</option>
+                        <option value="img-w">Image To Word</option>
+                    </select>
+                </li>
+            </ol>
+        </nav>
+    );
+    function setLevel(e) {
+        if (e.target.innerText.toLowerCase() == 'hard') {
+            // console.log(EasyLevel.current);
+            EasyLevel.current.classList.remove('level');
+            hardLevel.current.classList.add('level');
+        }else{
+            EasyLevel.current.classList.add('level');
+            hardLevel.current.classList.remove('level');
+        }
+    }
 }
 export default Nav;
